ec50751658b73d3095223c5038e712522f5ec383
Pavel <kurilo.pavel@mail.ru>
2024-05-25 03:04:46 +0300

    change url

--- src/app/constant.mjs
+++ src/app/constant.mjs
@@ -1,5 +1,5 @@
 // const ServerUrl = "http://localhost:7780";
-const ServerUrl = "https://test-paralect.onrender.com";
+const ServerUrl = "https://test-paralect-server.onrender.com";
 // const FrontUrl = "http://localhost:3000";
-const FrontUrl = "https://test-paralect-server.onrender.com";
+const FrontUrl = "https://test-paralect.onrender.com";
 export {ServerUrl, FrontUrl};
